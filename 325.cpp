285f70af6ec694c24556fe84f9e7f471f778cc56
Ajaysvasan <ajay192006@gmail.com>
2025-03-16 20:39:09 +0530

    325

--- 325.cpp
+++ 325.cpp
@@ -1,31 +1,33 @@
 #include<vector>
-#include<unordered_set>
+#include<unordered_map>
 #include<iostream>
 
 using namespace std;
 
-class Solution{
+class Solution {
     public:
-    int maxSubArraySize(vector<int>&nums,int k){
-        int res = 0;
-        for(int i = 0;i<nums.size();++i){
-            int sum = 0;
-            for(int j = i;j<nums.size();++j){
-                sum += nums[j];
-                if (sum == k){
-                    int subLen = j - i + 1;
-                    res = max(subLen,res);
-                }
+        int maxSubArrayLen(vector<int>& nums, int k) {
+            unordered_map<int, int> prefixMap;
+            int sum = 0, maxLen = 0;
+    
+            for(int i = 0; i < nums.size(); ++i) {
+                sum += nums[i];
+                if(sum == k)
+                    maxLen = i + 1;
+                if(prefixMap.count(sum - k))
+                    maxLen = max(maxLen, i - prefixMap[sum - k]);
+                if(!prefixMap.count(sum))
+                    prefixMap[sum] = i;
             }
+            return maxLen;
         }
-        return res;
-    }
-};
+    };
+    
 
 main(){
     Solution s;
     vector<int> arr = {10, 5, 2, 7, 1, -10};
     int k = 15;
-    cout << s.maxSubArraySize(arr, k) << endl;
+    cout << s.maxSubArrayLen(arr, k) << endl;
     return 0;
 }
